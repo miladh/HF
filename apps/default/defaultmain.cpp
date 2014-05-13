ee89b145bddfc3a3f3efca286df0631df9c1d3a7
Milad Hobbi Mobarhan <miladh@fys.uio.no>
2014-05-13 18:21:10 +0200

    added new infiles

--- apps/default/defaultmain.cpp
+++ apps/default/defaultmain.cpp
@@ -81,6 +81,8 @@ int main(int argc, char **argv)
         break;
     }
 
+
+
     int maxNumOfIteration = root["solverSettings"]["maxNumOfIteration"];
     double dampingFactor = root["solverSettings"]["dampingFactor"];
 
